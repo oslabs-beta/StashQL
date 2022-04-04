5445c494212543b60344c82378c54d8b29f156be
simonchn160 <simonchn160@gmail.com>
2022-04-04 16:37:21 -0400

    update package and add cli

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
-const { graphqlHTTP } = require('express-graphql');
 const { graphql } = require('graphql');
-const express = require('express');
-const app = express();
-const redis = require('redis');
-const { RedisSearchLanguages } = require('@node-redis/search/dist/commands');
 const fs = require('fs');
 const path = require('path');
 
@@ -22,20 +17,21 @@ class stashql {
 
 
   async queryHandler(req, res, next) {      
-    console.log("cwd: ", process.cwd()); // -> "/Users/Simon/dirname-example/StashQL package
+    // console.log("cwd: ", process.cwd()); // -> "/Users/Simon/dirname-example/StashQL package
     if (!fs.existsSync(path.join(process.cwd(), "logs"))){
       fs.mkdirSync(path.join(process.cwd(), "logs"), (err) => {
         if (err) {
-          console.log('could not make directory');
+          console.log('Error, could not make logs directory: ', err);
+          return next();
         }
       });
     }
     this.startTime = performance.now();
-    console.log('request:');
-    console.log(req.body.query);
+    // console.log('request:');
+    // console.log(req.body.query);
     this.query = req.body.query;
     const query = req.body.query;
-    if (query.slice(0, 8) === 'mutation') console.log('is a mutation');
+    // if (query.slice(0, 8) === 'mutation') console.log('is a mutation');
 
     //if the client did not submit a mutation (just a normal query)
     if (query.slice(0, 8) !== 'mutation') {
@@ -47,6 +43,7 @@ class stashql {
           const data = await this.cache.get(this.query);
           res.locals.data = JSON.parse(data);
           this.endTime = performance.now();
+          res.locals.runTime = this.endTime - this.startTime;
           console.log('performance: ', this.endTime - this.startTime);
           fs.appendFileSync(
             path.join(process.cwd(), "/logs/logs.txt"), 
@@ -70,6 +67,7 @@ class stashql {
               //we send back the data
               res.locals.data = JSON.parse(data);
               this.endTime = performance.now();
+              res.locals.runTime = this.endTime - this.startTime;
               console.log('performance: ', this.endTime - this.startTime);
               fs.appendFileSync(
                 path.join(process.cwd(), "/logs/logs.txt"), 
@@ -79,10 +77,11 @@ class stashql {
               return next();
             })
             .catch((error) => {
-              console.log('error in else block of queryHandler not mutation: ', error);
+              console.log('Error in else block of queryHandler for mutations: ', error);
             });
         }
       } catch (error) {
+        console.log('Error in running query: ', error);
         return next(error);
       }
     } 
@@ -92,7 +91,7 @@ class stashql {
       try {
         //we run the mutation and get the data back
         const data = JSON.stringify(await graphql({schema: this.schema, source: this.query}));
-        console.log('the result data: ', data);
+        // console.log('the result data: ', data);
 
         //we then check to see if the client use the refillCache argument - this means that the client wants to re-run all the queries in our cache
         //that matches the field the client passed in and update their corresponding values (data) in the cache
@@ -121,6 +120,7 @@ class stashql {
         }
         res.locals.data = JSON.parse(data);
         this.endTime = performance.now();
+        res.locals.runTime = this.endTime - this.startTime;
         console.log('performance: ', this.endTime - this.startTime);
         fs.appendFileSync(
           path.join(process.cwd(), "/logs/logs.txt"), 
@@ -128,7 +128,8 @@ class stashql {
           err => { if (err) {console.error(err)}});
         return next();
       } catch (error) {
-        return next(error);
+        console.log('Error in running mutation: ', error);
+        return next();
       }
     } 
   }
@@ -141,13 +142,13 @@ class stashql {
 
   //updating/deleting - updates all queries whose fields matches the field passed in
   async refillCacheHandler(field) {
-    console.log('refillCacheHandler invoked');
-    console.log('the field: ', field);
+    // console.log('refillCacheHandler invoked');
+    // console.log('the field: ', field);
     //we get all the keys and assign it to queryKeys (it is an array of keys)
     const queryKeys = await this.cache.keys('*');
     //iterate through the array of query keys 
     for (let queryKey of queryKeys) {
-      console.log('current queryKey in refillCacheHandler: ', queryKey);
+      // console.log('current queryKey in refillCacheHandler: ', queryKey);
       const secondCurly = queryKey.indexOf('{', 1);
       const currQueryField =  queryKey.slice(1, secondCurly).trim();
       //for each query key, we check to see if the field in that query matches the field passed in
@@ -156,7 +157,7 @@ class stashql {
         await graphql({schema: this.schema, source: queryKey})
           .then((data) => JSON.stringify(data))
           .then((data) => {
-            console.log(data);
+            // console.log(data);
             this.cache.set(queryKey, data);
             if (this.ttl !== undefined) {
               this.cache.expire(queryKey, this.ttl);
@@ -183,93 +184,6 @@ class stashql {
     }
   }
 
-  //creating/adding - only updates 1 query that they specify
-  // async updateSingleQuery(query, newData) {
-  //   //find the matching key in the cache
-  //   //take its value
-  //   const oldData = this.cache.get(query).data
-  //   const updatedData = [...oldData, newData.name]
-  // }
-  //add a book called "New Book"
-  //query: {
-      //     authors {
-      //       name
-      //       books {
-      //         id
-      //         name
-      //       }
-      //     }
-      //   }
-
-  // newData: { authorId: 1, name: "New Book" }
-  // oldData: {
-    // "data": {
-    //   "authors": [
-    //       {
-    //           "name": "J. K. Bowling",
-    //           "books": [
-    //               {
-    //                   "id": 1,
-    //                   "name": "Harry Potter and the Chamber of Secrets"
-    //               },
-    //               {
-    //                   "id": 2,
-    //                   "name": "Harry Potter and the Prisoner of Azkaban"
-    //               },
-    //               {
-    //                   "id": 3,
-    //                   "name": "Harry Potter and the Goblet of Fire"
-    //               }
-    //           ]
-    //       }, {
-                
-    //}
-    //   ]
-    // }
-
 }
 
 module.exports = stashql;
-
-  //we get the query
-  //first check to see if it is a mutation
-    //if so, run the mutation and get the data back
-    //then check the arguments passed in the mutation for an updateQueries or updateSingleQuery property - this will be the field whose data you want to update
-    //if there is an updateQueries property
-      //iterate through each key in the cache and for each key, if it includes the field passed in, re-run that query
-    //if there is an updateSingleQuery property:
-      //then find that query in the cache, get its corresponding value (old data), and append the new data returned from the mutation to it
-      //and set this as the query's new value
-  
-  //if NOT a mutation:
-  //we check if it is in the cache
-    //if so, just return the cached data back to client
-    //if not, run the query as normal and store the query in cache along with the returned data
-
-
-
-              // const startingIdx = query.indexOf('updateSingleQuery');
-          // const leftHalf = query.slice(0, startingIdx);
-          // const parenIdx = leftHalf.indexOf('(');
-          // const properties = leftHalf.slice(parenIdx + 1, startingIdx - 2);
-          // console.log('LOOK HERE: ', properties);
-
-          // function looseJsonParse(data) {
-          //   return Function('"use strict";return (' + data + ')')();
-          // }
-          
-          // const data = `{${properties}}`;
-          // const newData = JSON.stringify(looseJsonParse(data));
-          // const json = JSON.parse(newData);
-          // console.log("JSON: ", json)
-          // console.log('TYPE OF JSON: ', typeof json);
-
-
-//create function that creates file(maybe folder) to log queries and stuff (user enters file path or we make our own) - DONE
-
-
-
-//first create a cli 
-//when user types in command "logs *", it would display all logs
-//can try to implement some kind of command that would filter out logs by date/time/query
-//allow users to enter log db
