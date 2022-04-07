6d95d82ee6bebe0c833759f179213ba6c98bcab7
Ian <ianfmadden@gmail.com>
2022-04-07 16:12:52 -0500

    Removed DB connection as not currently used.

--- stashQL-demo/server/models/db.js
+++ stashQL-demo/server/models/db.js
@@ -2,11 +2,11 @@ const { Pool } = require('pg');
 // db.js
 // import postgres from 'postgres'
 
-const PG_URI = 'postgres://tkqniwuc:k0nfCsvem7yAZYEGREAoHwRUpibxBN9o@ruby.db.elephantsql.com/tkqniwuc';
+// const PG_URI = 'postgres://tkqniwuc:k0nfCsvem7yAZYEGREAoHwRUpibxBN9o@ruby.db.elephantsql.com/tkqniwuc';
 
-const pool = new Pool({
-  connectionString: PG_URI,
-});
+// const pool = new Pool({
+//   connectionString: PG_URI,
+// });
 
 module.exports = {
   query: (text, params, callback) => {
