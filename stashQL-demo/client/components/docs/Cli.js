4877e150212e6b5ea5ee414207352f7803e72e24
simonchn160 <simonchn160@gmail.com>
2022-04-10 17:57:48 -0400

    edit cli layout

--- stashQL-demo/client/components/docs/Cli.js
+++ stashQL-demo/client/components/docs/Cli.js
@@ -8,7 +8,8 @@ const Cli = () => {
       <div className='cli-text'>
         <p>Upon running your GraphQL queries, StashQL will create a logs file that will contain detailed information about each query and mutation you run.</p>
         <p>To view the logs, you can use StashQL's CLI.</p>
-        <p><Highlighter highlightClassName="YourHighlightClass" searchWords={["stashql all logs"]} autoEscape={true} textToHighlight="stashql all logs"/></p>  
+        <p><Highlighter highlightClassName="YourHighlightClass" searchWords={["stashql all logs"]} autoEscape={true} textToHighlight="stashql all logs"/></p> 
+        
       </div>
     </div>
   )
